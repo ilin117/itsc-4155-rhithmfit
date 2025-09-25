7d5f4184e4047aabee955ae1afe3af0bc6a03cfb
[ilin117] <[ilin1172004@gmail.com]>
2025-09-25 12:31:33 -0400

    added colors

--- app/src/main/java/com/example/rhithmfit/MainActivity.java
+++ app/src/main/java/com/example/rhithmfit/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.rhithmfit.fragments.LoginFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,5 +24,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main, new LoginFragment()).commit();
     }
 }
---
+++ app/src/main/java/com/example/rhithmfit/fragments/LoginFragment.java
@@ -0,0 +1,35 @@
+package com.example.rhithmfit.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rhithmfit.R;
+import com.example.rhithmfit.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+    FragmentLoginBinding binding;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
